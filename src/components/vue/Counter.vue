2a9a46267293fd5873fa2c4c2aab44e09b30650c
Andresss <davaccar@ufpso.edu.co>
2024-04-25 13:03:35 -0500

    Counter button

---
+++ src/components/vue/Counter.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import {ref} from 'vue'
+    let count = ref(0);
+
+    function addvalue(){
+        count.value++;
+        console.log(count)
+    }
+</script>
+
+<template>
+    <div @click="addvalue()" class="bg-green-500 w-30 h-10 px-4 py-2 rounded-md text-white font-semibold inline-block hover:cursor-pointer hover:bg-green-600 transition-colors">
+        ¡Press me! {{ count }}
+    </div>
+</template>
